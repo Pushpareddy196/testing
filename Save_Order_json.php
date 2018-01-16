35b4999ad16e1eb011fbfd356ba99a2de42f86e9
Pushpareddy196 <35150237+Pushpareddy196@users.noreply.github.com>
2018-01-16 22:46:57 +0530

    Update Save_Order_json.php

--- Save_Order_json.php
+++ Save_Order_json.php
@@ -1,5 +1,17 @@
 <?php
-    $fp = fopen('orders.json', 'a');
-    fwrite($fp, json_encode($_POST['neworder']));
-    fclose($fp);
+  if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
+      $inp = file_get_contents('orders.json');
+     $tempArray = json_decode($inp, true);
+     
+      array_push( $tempArray ['orders'], $_POST['neworder'][0]);
+    $jsonData = json_encode($tempArray);
+    
+    if (json_decode($jsonData) != null)
+          {
+    $fp = fopen('orders.json', 'w');
+     fwrite($fp, $jsonData);
+  fclose($fp);
+          }
+     
+  }
